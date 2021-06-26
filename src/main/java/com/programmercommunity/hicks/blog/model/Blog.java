ac91923ecf637802235b13b9f017fd9460ac0564
programmercommunity-manimaran <programmercommunity.manimaran@gmail.com>
2021-06-26 12:15:07 +0530

    blog manipulation api

--- src/main/java/com/programmercommunity/hicks/blog/model/Blog.java
+++ src/main/java/com/programmercommunity/hicks/blog/model/Blog.java
@@ -8,117 +8,101 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("blog")
 public class Blog {
 	@Id
-	public String id;
-	public String title;
-	public String subtitle;
-	public String authorId; // user id
-	public List<String> images;
-	public List<String> contents;
-	public long views;
-	public long likes;
-	public long commentCount; // no of comments
-	public String addedDate;
-	public String lastActiveDate;
-	public List<String> hashtags;
-
+	private String id;
+	private String title;
+	private String subtitle;
+	private String authorId; // user id
+	private List<String> images;
+	private List<String> contents;
+	private List<String> hashtags;
+	private long views;
+	private long likes;
+	private long noOfComment; // no of comments
+	private boolean isPublic;
+	private String addedDate;
+	private String lastActiveDate;
+	
 	public String getId() {
-		return this.id;
+		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getTitle() {
-		return this.title;
+		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getSubtitle() {
-		return this.subtitle;
+		return subtitle;
 	}
-
 	public void setSubtitle(String subtitle) {
 		this.subtitle = subtitle;
 	}
-
 	public String getAuthorId() {
-		return this.authorId;
+		return authorId;
 	}
-
 	public void setAuthorId(String authorId) {
 		this.authorId = authorId;
 	}
-
 	public List<String> getImages() {
-		return this.images;
+		return images;
 	}
-
 	public void setImages(List<String> images) {
 		this.images = images;
 	}
-
 	public List<String> getContents() {
-		return this.contents;
+		return contents;
 	}
-
 	public void setContents(List<String> contents) {
 		this.contents = contents;
 	}
-
-	public long getViews() {
-		return this.views;
-	}
-
-	public void setViews(long views) {
-		this.views = views;
-	}
-
-	public long getLikes() {
-		return this.likes;
-	}
-
-	public void setLikes(long likes) {
-		this.likes = likes;
-	}
-
-	public long getCommentCount() {
-		return this.commentCount;
-	}
-
-	public void setCommentCount(long commentCount) {
-		this.commentCount = commentCount;
-	}
-
-	public String getAddedDate() {
-		return this.addedDate;
-	}
-
-	public void setAddedDate(String addedDate) {
-		this.addedDate = addedDate;
-	}
-
-	public String getLastActiveDate() {
-		return this.lastActiveDate;
-	}
-
-	public void setLastActiveDate(String lastActiveDate) {
-		this.lastActiveDate = lastActiveDate;
-	}
-
 	public List<String> getHashtags() {
-		return this.hashtags;
+		return hashtags;
 	}
-
 	public void setHashtags(List<String> hashtags) {
 		this.hashtags = hashtags;
 	}
-
+	public long getViews() {
+		return views;
+	}
+	public void setViews(long views) {
+		this.views = views;
+	}
+	public long getLikes() {
+		return likes;
+	}
+	public void setLikes(long likes) {
+		this.likes = likes;
+	}
+	public long getNoOfComment() {
+		return noOfComment;
+	}
+	public void setNoOfComment(long noOfComment) {
+		this.noOfComment = noOfComment;
+	}
+	public boolean isPublic() {
+		return isPublic;
+	}
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	public String getAddedDate() {
+		return addedDate;
+	}
+	public void setAddedDate(String addedDate) {
+		this.addedDate = addedDate;
+	}
+	public String getLastActiveDate() {
+		return lastActiveDate;
+	}
+	public void setLastActiveDate(String lastActiveDate) {
+		this.lastActiveDate = lastActiveDate;
+	}
 	public Blog(String id, String title, String subtitle, String authorId, List<String> images, List<String> contents,
-			long views, long likes, long commentCount, String addedDate, String lastActiveDate, List<String> hashtags) {
+			List<String> hashtags, long views, long likes, long noOfComment, boolean isPublic, String addedDate,
+			String lastActiveDate) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -126,16 +110,16 @@ public class Blog {
 		this.authorId = authorId;
 		this.images = images;
 		this.contents = contents;
+		this.hashtags = hashtags;
 		this.views = views;
 		this.likes = likes;
-		this.commentCount = commentCount;
+		this.noOfComment = noOfComment;
+		this.isPublic = isPublic;
 		this.addedDate = addedDate;
 		this.lastActiveDate = lastActiveDate;
-		this.hashtags = hashtags;
 	}
-
 	public Blog() {
 		super();
 	}
-
+	
 }
